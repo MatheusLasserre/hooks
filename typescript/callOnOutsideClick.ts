f93cd81bf3d60c7ceb2b6f0de6f0315d5874d870
Matheus Lasserre <matheuslasserre3@gmail.com>
2024-01-13 15:28:22 -0300

    callOnOutSideClick

---
+++ typescript/callOnOutsideClick.ts
@@ -0,0 +1,21 @@
+import { useEffect, useRef } from 'react'
+
+export const callOnOutsideClick = (trigger: () => void) => {
+  const ref = useRef(null)
+
+  const handleClickOutside = (event: Event) => {
+    // @ts-expect-error - TS complains about ref.current being null, but it's just to prevent a React bug
+    if (ref.current && !ref.current.contains(event.target)) {
+      trigger()
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutside, true)
+    return () => {
+      document.removeEventListener('click', handleClickOutside, true)
+    }
+  }, [])
+
+  return ref
+}
